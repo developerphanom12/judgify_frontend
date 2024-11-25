1e7df904d39f3cddc0778db70b1bceb6c7cba91d
developerphanom12 <developer.phanomprofessionals@gmail.com>
2024-11-25 19:00:34 +0530

    user

--- src/Url/Url.jsx
+++ src/Url/Url.jsx
@@ -1,9 +1,8 @@
-// export const EXCHNAGE_URL = 'https://judgify-api.phanomprofessionals.com/api/admin'
 
-// export const EXCHNAGE_URL = 'https://judgify-api.phanomprofessionals.com/api/admin'
-export const EXCHNAGE_URL = 'http://localhost:3600/api/admin'
+export const EXCHNAGE_URL = 'https://judgify-api.phanomprofessionals.com/api/admin'
+// export const EXCHNAGE_URL = 'http://localhost:3600/api/admin'
 
 
-// export const IMAGES_URL = 'https://judgify-api.phanomprofessionals.com/uploads/'
-export const IMAGES_URL = 'http://localhost:3600/uploads/'
+export const IMAGES_URL = 'https://judgify-api.phanomprofessionals.com/uploads/'
+// export const IMAGES_URL = 'http://localhost:3600/uploads/'
 
